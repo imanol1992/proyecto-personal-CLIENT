25e90af8a0de78bf7a73287a31fec237dd96da3f
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2020-07-15 01:22:56 +0200

    proyecto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  //comprobar a quitar el React.strictMode
+  <>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/utils/formValidation.js
@@ -0,0 +1,37 @@
+export function minLengthValidation(inputData, minLength) {
+  const { value } = inputData;
+
+    removeClassErrorSucces(inputData);
+    
+    if (value.length >= minLength) {
+        inputData.classList.add("success");
+        return true;  
+    } else {
+        inputData.classList.add("error");
+        return false;
+    }
+}
+
+export function emailValidation(inputData) {
+    //expresion regular para validar emails
+    // eslint-disable-next-line no-useless-escape
+    const emailValid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const { value } = inputData;
+
+    removeClassErrorSucces(inputData);
+
+    const resultValidation = emailValid.test(value);
+    if (resultValidation) {
+        inputData.classList.add("success");
+        return true;
+    } else {
+        inputData.classList.add("error");
+        return false;
+     }
+}
+
+function removeClassErrorSucces(inputData) {
+  inputData.classList.remove("success");
+  inputData.classList.remove("error");
+}
+
